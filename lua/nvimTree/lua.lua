81ba44d7386a1da63340f9ff567b43be54143976
Sergio Alejandro Ribera Costa <sergioalejandroriberacosta@gmail.com>
2021-06-23 01:48:25 -0400

    Refactor and fix

--- lua/nvimTree/lua.lua
+++ lua/nvimTree/lua.lua
@@ -1,8 +1,6 @@
 local cmd = vim.cmd
 local g = vim.g
 
-cmd [[packadd nvim-tree.lua]]
-
 vim.o.termguicolors = true
 
 g.nvim_tree_side = "left"
@@ -21,68 +19,75 @@ g.nvim_tree_allow_resize = 1
 g.nvim_tree_width_allow_resize  = 0
 g.nvim_tree_lsp_diagnostics = 1
 g.nvim_tree_special_files = { 'README.md', 'Makefile', 'MAKEFILE' }
+g.nvim_tree_window_picker_exclude = {
+    filetype = {
+        'packer',
+        'qf'
+    },
+    buftype = {
+        'terminal'
+    }
+}
 g.nvim_tree_show_icons = {
-  git = 1,
-  folders = 1,
-  files = 1
+    git = 1,
+    folders = 1,
+    files = 1
 }
 
 g.nvim_tree_icons = {
-  default = " ",
-  symlink = " ",
-  git = {
-    unstaged = "✗",
-    staged = "✓",
-    unmerged = "",
-    renamed = "➜",
-    untracked = "★"
-  },
-  folder = {
-    default = "",
-    open = "",
-    symlink = ""
-  },
-  lsp = {
-    hint = "",
-    info = "",
-    warning = "",
-    error = "",
-  }
+    default = " ",
+    symlink = " ",
+    git = {
+        unstaged = "✗",
+        staged = "✓",
+        unmerged = "",
+        renamed = "➜",
+        untracked = "●"
+    },
+    folder = {
+        default = "",
+        open = "",
+        symlink = ""
+    },
+    lsp = {
+        hint = "",
+        info = "",
+        warning = "",
+        error = "",
+    }
 }
 
-local get_lua_cb = function(cb_name)
-  return string.format(":lua require'nvim-tree'.on_keypress('%s')<CR>", cb_name)
-end
-
 -- Mappings for nvimtree
-
 vim.api.nvim_set_keymap(
-  "n",
-  "<leader>n",
-  ":NvimTreeToggle<CR>",
-  {
+"n",
+"<leader>n",
+":NvimTreeToggle<CR>",
+{
     noremap = true,
     silent = true
-  }
+}
 )
 
+local tree_cb = require'nvim-tree.config'.nvim_tree_callback
+
 g.nvim_tree_bindings = {
-    ["o"] = get_lua_cb("edit"),
-    ["<2-LeftMouse>"] = get_lua_cb("edit"),
-    ["v"] = get_lua_cb("vsplit"),
-    ["s"] = get_lua_cb("split"),
-    ["t"] = get_lua_cb("tabnew"),
-    ["I"] = get_lua_cb("toggle_ignored"),
-    ["H"] = get_lua_cb("toggle_dotfiles"),
-    -- ["R"] = get_lua_cb("refresh"),
-    ["a"] = get_lua_cb("create"),
-    ["d"] = get_lua_cb("remove"),
-    ["r"] = get_lua_cb("rename"),
-    ["x"] = get_lua_cb("cut"),
-    ["c"] = get_lua_cb("copy"),
-    ["p"] = get_lua_cb("paste"),
-    ["-"] = get_lua_cb("dir_up"),
-    ["q"] = get_lua_cb("close")
+    ["o"] = tree_cb("edit"),
+    ["<2-LeftMouse>"] = tree_cb("edit"),
+    ["v"] = tree_cb("vsplit"),
+    ["s"] = tree_cb("split"),
+    ["t"] = tree_cb("tabnew"),
+    ["C"] = tree_cb("cd"),
+    ["I"] = tree_cb("toggle_ignored"),
+    ["H"] = tree_cb("toggle_dotfiles"),
+    ["R"] = tree_cb("refresh"),
+    ["a"] = tree_cb("create"),
+    ["d"] = tree_cb("remove"),
+    ["r"] = tree_cb("rename"),
+    ["x"] = tree_cb("cut"),
+    ["c"] = tree_cb("copy"),
+    ["p"] = tree_cb("paste"),
+    ["-"] = tree_cb("dir_up"),
+    ["q"] = tree_cb("close")
 }
 
 cmd "hi NvimTreeFolderIcon guifg = #61afef"
