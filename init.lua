50102c60ea3f58254d2cb791711790c6243eb15e
Sergio Alejandro Ribera Costa <sergioalejandroriberacosta@gmail.com>
2021-06-23 01:46:24 -0400

    Refactor: Move set variables into misc folder

--- init.lua
+++ init.lua
@@ -1,62 +1,41 @@
--- misc-utils.oneinstance"
--- load all plugins
 require "pluginsList.lua"
 require "file-icons.lua"
 
 require "misc-utils.lua"
--- require "bufferline.lua"
-require "lualine.lua"
+require "tabline.lua".setup {
+    show_index = false,
+    show_modify = true,
+    show_icon = true,
+    show_close = false,
+    separator = '',
+    spacing = '',
+    indicators = {
+        modify = '●'
+    },
+    no_name = '[No Name]',
+    colors = {
+        selected = {
+            bg = "#81A1C1",
+            fg = "#1e222a"
+        },
+        disabled = {
+            bg = "#72767b",
+            -- bg = "#808892",
+            fg = "#1e222a"
+        },
+        empty = "#282c34"
+    }
+}
+require "statusline.lua"
 
 require("colorizer").setup()
 
 require"surround".setup{}
-require("kommentary.lua")
 
 -- lsp
 require "lsp.init"
 require "completion.lua"
-
-local cmd = vim.cmd
-local g = vim.g
-
-g.instant_username = "s4rch"
-g.vsnip_snippet_dir = "~/.config/nvim/snippets"
-g.mapleader = " "
-g.auto_save = 0
-g.kommentary_create_default_mappings = false
-
--- colorscheme related stuff
-cmd "syntax enable"
-cmd "syntax on"
-
--- local base16 = require "base16"
--- base16(base16.themes["material"], true)
-
--- blankline
-
-g.indentLine_enabled = 1
-g.indentLine_char_list = { "▏", '¦', '┆', '┊' }
-g.ident_blankline_ident_level = 4
--- g.indent_blankline_char = "▏"
-
---
---  Neovide Configurations
---
--- g.neovide_transparency=0.8                  " Neovide Transparency
-g.neovide_cursor_antialiasing = true        -- Nevovide cursor Antialiasing
--- g.neovide_cursor_vfx_mode = "railgun"       -- Neovide Efect on Move Cursor
-g.neovide_cursor_vfx_mode = "ripple"        -- Neovide
--- set guifont=Inconsolata:h15
--- set guifont=NotoSansMono:h15
--- set guifont=Hack:15;Iosevka:15;NotoSansMono:h15
-
-
-g.indent_blankline_filetype_exclude = {"help", "terminal"}
-g.indent_blankline_buftype_exclude = {"terminal"}
-
-g.indent_blankline_show_trailing_blankline_indent = false
-g.indent_blankline_show_first_indent_level = false
-
+require "comments.lua"
 require "mappings.lua"
 require "treesitter.lua"
 
@@ -68,6 +47,7 @@ require "instant"
 require "gitsigns.lua"
 
 require("nvim-autopairs").setup()
+require("nvim-autopairs").disable()
 
 require("lspkind").init(
 {
@@ -82,3 +62,4 @@ require("lspkind").init(
 vim.api.nvim_exec([[
 au BufEnter term://* setlocal nonumber
 ]], false)
+
--- lua/misc-utils/lua.lua
+++ lua/misc-utils/lua.lua
@@ -1,3 +1,44 @@
+local cmd = vim.cmd
+local g = vim.g
+
+g.instant_username = "s4rch"
+g.vsnip_snippet_dir = "~/.config/nvim/snippets"
+g.mapleader = " "
+g.auto_save = 0
+g.kommentary_create_default_mappings = false
+
+-- colorscheme related stuff
+cmd "syntax enable"
+cmd "syntax on"
+
+-- local base16 = require "base16"
+-- base16(base16.themes["material"], true)
+
+-- blankline
+
+g.indentLine_enabled = 1
+g.indentLine_char_list = { "▏", '¦', '┆', '┊' }
+g.ident_blankline_ident_level = 4
+-- g.indent_blankline_char = "▏"
+
+--
+--  Neovide Configurations
+--
+-- g.neovide_transparency=0.8                  " Neovide Transparency
+g.neovide_cursor_antialiasing = true        -- Nevovide cursor Antialiasing
+-- g.neovide_cursor_vfx_mode = "railgun"       -- Neovide Efect on Move Cursor
+g.neovide_cursor_vfx_mode = "ripple"        -- Neovide
+-- set guifont=Inconsolata:h15
+-- set guifont=NotoSansMono:h15
+-- set guifont=Hack:15;Iosevka:15;NotoSansMono:h15
+
+
+g.indent_blankline_filetype_exclude = {"help", "terminal"}
+g.indent_blankline_buftype_exclude = {"terminal"}
+
+g.indent_blankline_show_trailing_blankline_indent = false
+g.indent_blankline_show_first_indent_level = false
+
 local scopes = {o = vim.o, b = vim.bo, w = vim.wo}
 
 local function opt(scope, key, value)
