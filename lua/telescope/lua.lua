e33c9c5ba24468fe3af431a055e40baeafa6d0f2
Sergio Alejandro Ribera Costa <sergioalejandroriberacosta@gmail.com>
2021-07-25 16:43:36 -0400

    Fix: Fix bug with telescope

--- lua/telescope/lua.lua
+++ lua/telescope/lua.lua
@@ -1,23 +1,18 @@
-local finders = require('telescope.finders')
-local make_entry = require('telescope.make_entry')
-local pickers = require('telescope.pickers')
-local utils = require('telescope.utils')
-local conf = require('telescope.config').values
-local M = {}
-
 require("telescope").setup {
     defaults = {
         vimgrep_arguments = {
-            "rg",
-            "--color=never",
-            "--no-heading",
-            "--with-filename",
-            "--line-number",
-            "--column",
-            "--smart-case"
+            'rg',
+            '--color=never',
+            '--no-heading',
+            '--with-filename',
+            '--line-number',
+            '--column',
+            '--smart-case'
         },
-        prompt_prefix = " ",
-        selection_caret = " ",
+        prompt_prefix = "❯ ",
+        selection_caret = "❯ ",
+        -- prompt_prefix = " ",
+        -- selection_caret = " ",
         entry_prefix = "  ",
         initial_mode = "insert",
         selection_strategy = "reset",
@@ -26,27 +21,27 @@ require("telescope").setup {
         layout_config = {
             horizontal = {
                 mirror = false,
-                preview_width = 0.5
             },
             vertical = {
-                mirror = false
-            }
+                mirror = false,
+            },
         },
-        file_sorter = require "telescope.sorters".get_fuzzy_file,
+        file_sorter =  require'telescope.sorters'.get_fuzzy_file,
         file_ignore_patterns = {},
-        generic_sorter = require "telescope.sorters".get_generic_fuzzy_sorter,
-        path_display = true,
+        generic_sorter =  require'telescope.sorters'.get_generic_fuzzy_sorter,
         winblend = 0,
         border = {},
-        borderchars = {"─", "│", "─", "│", "╭", "╮", "╯", "╰"},
+        borderchars = { '─', '│', '─', '│', '╭', '╮', '╯', '╰' },
         color_devicons = true,
         use_less = true,
-        set_env = {["COLORTERM"] = "truecolor"}, -- default = nil,
-        file_previewer = require "telescope.previewers".vim_buffer_cat.new,
-        grep_previewer = require "telescope.previewers".vim_buffer_vimgrep.new,
-        qflist_previewer = require "telescope.previewers".vim_buffer_qflist.new,
+        path_display = {},
+        set_env = { ['COLORTERM'] = 'truecolor' }, -- default = nil,
+        file_previewer = require'telescope.previewers'.vim_buffer_cat.new,
+        grep_previewer = require'telescope.previewers'.vim_buffer_vimgrep.new,
+        qflist_previewer = require'telescope.previewers'.vim_buffer_qflist.new,
+
         -- Developer configurations: Not meant for general override
-        buffer_previewer_maker = require "telescope.previewers".buffer_previewer_maker
+        buffer_previewer_maker = require'telescope.previewers'.buffer_previewer_maker
     },
     extensions = {
         media_files = {
@@ -55,6 +50,7 @@ require("telescope").setup {
         }
     }
 }
+
 require("telescope").load_extension("media_files")
 
 local opt = {noremap = true, silent = true}
